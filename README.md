a395b863a9aba5415caabc6a581e639f525758a2
shawn <1803089925@qq.com>
2020-04-27 15:18:01 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,5 @@
+## me的vuepress
+
+### 参考文件
+1. [搭建一个类Vue文档风格的技术文档/博客](https://segmentfault.com/a/1190000016333850)
+2. [vuePress官网](https://vuepress.vuejs.org/zh/guide/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'Hello shinan',
+  description: 'just paly',
+  base: '/',
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    nav: [
+      { text: '前端算法', link: '/algorithm/' },
+      { text: '博客', link: 'http://www.baidu.com' },
+      {
+        text: 'Github',
+        items: [
+          {text: 'GitHub地址', link: 'https://github.com/shinan96'},
+          {text: '算法仓库', link: 'https://github.com/shinan96'},
+        ]
+      }
+    ],
+  },
+}
